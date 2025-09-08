8e76c319d3bfbd7180d9d2a42aae90f336e1809d
Gamerk786po <abdullahpper@gmail.com>
2025-09-09 00:37:15 +0500

    Controllers and routes setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-import express, { urlencoded } from "express";
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
@@ -12,5 +13,11 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+// Routers import
+import userRouter from "./routes/user.route.js";
+
+// Routers declaration
+app.use("/api/v1/user", userRouter);
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 import dbConnect from "./db/dbconnect.js";
+import { app } from "./app.js";
 
-dbConnect();
+const PORT = process.env.PORT || 8000;
+
+dbConnect()
+  .then(() => {
+    app.listen( PORT , "0.0.0.0", () => {
+      console.log(`Server is runing that http://localhost:${PORT}.`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Mongo DB failed to connect ${error}`);
+  });
